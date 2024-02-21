e9f950af41a257a2c53a7257809a4df1695e5761
paulaamstutz <paula@saynode.ch>
2024-02-21 13:58:27 +0100

    image https://ibb.co/PNMPd49

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.get('/', (req, res) => {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta property="og:image" content="https://drive.google.com/file/d/1wfM1elF63KtwuSzekArKybVdBySBQ_Qj/view?usp=sharing">
+      <meta property="og:image" content="https://ibb.co/PNMPd49">
       <meta property="fc:frame" content="vNext">
-      <meta property="fc:frame:image" content="https://drive.google.com/file/d/1wfM1elF63KtwuSzekArKybVdBySBQ_Qj/view?usp=sharing">
+      <meta property="fc:frame:image" content="https://ibb.co/PNMPd49">
       
       <meta property="fc:frame:button:1" content="Social Media"/>
       <meta property="fc:frame:button:1:action" content="link"/>
