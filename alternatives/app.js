f8463324e5735da5c9828e0a2ebd1b67db896d4b
yann <yann@saynode.ch>
2024-02-26 18:04:14 +0100

    switch view

--- alternatives/app.js
+++ alternatives/app.js
@@ -5,8 +5,10 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   console.log('Request query:')
-  console.log(req.query);
-  res.send(`
+  console.log(req.query.id);
+  if (req.query.id == 1) {
+    console.log('Request query id is 1');
+    res.send(`
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -24,7 +26,7 @@ app.get('/', (req, res) => {
     
       <meta property="fc:frame:button:2" content="Decentralized Social Network"/>
       <meta property="fc:frame:button:2:action" content="link"/>
-      <meta property="fc:frame:button:2:target" content="https://www.skillbuddy.io/"/>
+      <meta property="fc:frame:button:2:target" content="https://frames-js-1-z6iq.onrender.com?id=2"/>
 
    
       <meta property="fc:frame:button:3" content="Smart Contract"/>
@@ -37,6 +39,32 @@ app.get('/', (req, res) => {
     </body>
     </html>
   `);
+  } else {
+    console.log('Request query id is not 1');
+    res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta property="og:image" content="https://i.ibb.co/KxN6kPW/skillbuddy-canva-1.png">
+      <meta property="fc:frame" content="vNext">
+      <meta property="fc:frame:image" content="https://i.ibb.co/KxN6kPW/skillbuddy-canva-1.png">
+      
+      <meta property="fc:frame:button:1" content="Subscribe"/>
+      <meta property="fc:frame:button:1:action" content="link"/>
+      <meta property="fc:frame:button:1:target" content="https://warpcast.com/skillbuddy"/>
+
+    </head>
+    <body>
+      <script type="text/javascript">
+        window.location.href = "https://www.skillbuddy.io/";
+      </script>
+    </body>
+    </html>
+  `);
+  }
+
   //res.send(html);
 });
 
